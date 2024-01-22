cf3e0ea9c71b701d9180b70ec24307be9b331d13
Mariia <99070745+uraraur@users.noreply.github.com>
2024-01-22 13:27:54 +0200

    вввв

---
+++ lab2.py
@@ -0,0 +1,91 @@
+import hashlib as hl
+import random
+#variant 4 --- sha256
+
+N = 10000
+K = [2**5, 2**6, 2**7]
+L = [2**5, 2**6, 2**7]
+
+n = 32 
+truncation = n // 8
+pad = 128 - n 
+
+#--------------------------------------------------------------------------------------------
+
+def generate_bytes(len):
+    return random.randbytes(len).hex()
+
+#r = generate_bytes(pad // 8)
+def R(x, r):
+    return (r + x)
+
+def gen_hash(t):
+    res = hl.sha256(bytes.fromhex(t)).hexdigest()
+    return res[len(res) - truncation : len(res)]
+
+#--------------------------------------------------------------------------------------------
+
+def generate_table(K, L):
+    r = generate_bytes(pad // 8)
+    X = {}
+
+    for i in range(K):
+        x_i0 = generate_bytes(n // 8)
+        x_ij = x_i0
+        for j in range(L): 
+            x_ij = gen_hash(R(x_ij, r))
+        X[x_ij] = x_i0
+
+    return X, r
+
+def preimage_search(K, L, X, hash_value):
+    y = hash_value
+    
+    for j in range(L):
+        if y in X:
+            x = X[y]
+            for m in range(L - j - 1):
+                x = gen_hash(R(x, r))
+            return R(x, r)
+        y = gen_hash(R(y, r))
+
+    return 0
+
+def multi_preimage_search(K, L, Xarr, hash_value):
+    y = [hash_value] * K
+    
+    for j in range(L):
+        i = 0
+        for (X, r) in Xarr:
+            if y[i] in X:
+                x = X[y[i]]
+                for m in range(L - j - 1):
+                    x = gen_hash(R(x, r))
+                return R(x, r)
+            y[i] = gen_hash(R(y[i], r))
+            i = i + 1
+
+    return 0
+
+
+#--------------------------------------------------------------------------------------------
+
+results = []
+success = 0
+tables = []
+for i in range(K[1]):
+    tables.append(generate_table(K[1], L[1]))
+for i in range(N):
+    x = generate_bytes(256 // 8)
+    h_x = gen_hash(x)
+    preim = multi_preimage_search(K[1], L[1], tables, h_x)
+    if preim != 0:
+        results.append((x, preim))
+        #print(f"i: {i}, x: {x}, h_x: {h_x}, preim: {preim}")
+
+for (x, p) in results:
+    if gen_hash(x) == gen_hash(p):
+        success = success + 1
+
+print(len(results))
+print(success)
